fe953c5276fe37e868fe9d7273b7f72981a50164
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-12-16 12:33:24 -0300

    chore: add link to redirect card

--- components/Card/index.vue
+++ components/Card/index.vue
@@ -15,14 +15,16 @@ defineProps({
 })
 </script>
 <template>
-  <div class="flex flex-col md:flex-row gap-4">
-    <div
-      class="w-100 md:min-w-[300px] h-[150px] bg-neutral-500 rounded-md"
-    ></div>
-    <div class="flex flex-col">
-      <span class="text-red-400">{{ label }}</span>
-      <span class="text-xl font-medium mb-2">{{ title }}</span>
-      <p class="text-neutral-400">{{ description }}</p>
+  <nuxt-link to="/">
+    <div class="flex flex-col md:flex-row gap-4">
+      <div
+        class="w-100 md:min-w-[300px] h-[150px] bg-neutral-500 rounded-md"
+      ></div>
+      <div class="flex flex-col">
+        <span class="text-red-400">{{ label }}</span>
+        <span class="text-xl font-medium mb-2">{{ title }}</span>
+        <p class="text-neutral-400">{{ description }}</p>
+      </div>
     </div>
-  </div>
+  </nuxt-link>
 </template>
