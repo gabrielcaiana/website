2bfb1c61d744e216c7e2eae9a58c893e78df1aa2
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-12-16 11:10:33 -0300

    feat: home

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <Header />
-  <div class="container mx-auto max-w-[52rem] my-10 text-white">
+  <div class="container mx-auto max-w-[52rem] my-[80px] text-white">
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>hello world</div>
+  <main class="flex flex-col gap-[80px]">
+    <section>
+      <h1 class="text-3xl font-medium mb-6 pb-3 border-b border-neutral-800">
+        Olá, Eu sou Gabriel Caiana
+      </h1>
+      <p>
+        Sou um engenheiro de software de 26 anos, especializado em tecnologias
+        Javascript, Vue, Web Performance e design.
+        <nuxt-link to="/about" class="text-blue-400 underline"
+          >Leia mais <Icon name="ph:arrow-right" size="14"
+        /></nuxt-link>
+      </p>
+    </section>
+
+    <section>
+      <h2 class="text-3xl font-medium mb-6 pb-3 border-b border-neutral-800">
+        Blog Posts
+      </h2>
+      <p>Em desenvolvimento</p>
+    </section>
+
+    <section>
+      <h3 class="text-3xl font-medium mb-6 pb-3 border-b border-neutral-800">
+        Projetos Recentes
+      </h3>
+      <p>Em desenvolvimento</p>
+    </section>
+  </main>
 </template>
